7aada70c098161d26abcd637d59642a0550f2c29
santoshir <hemantbly.kumar@gmail.com>
2018-05-03 16:41:15 +0530

    first commit

---
+++ Become_An_Instructor.php
@@ -0,0 +1,28 @@
+<?php
+$toEmail = "requests@itupworks.com";
+$msg = "";
+/* Handling Form Post */
+if($_SERVER['REQUEST_METHOD'] == 'POST' &&  $_POST['name'] !="" &&  $_POST['email'] !="" && $_POST['mobile'] !="" && $_POST['expertise'] !=""){
+        $body ="<br/>Name : ". $_POST['name'];
+        $body .="<br/>Email : ". $_POST['email'];
+		$body .="<br/>Mobile : ". $_POST['mobile'];
+		$body .="<br/>Expertise : ". $_POST['expertise'];
+        //$body .="<br/>Message : ". $_POST['message'];
+		$headers .= "MIME-Version: 1.0\r\n";
+        $headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
+		$subject='Area of Expertise'.$_POST['expertise'].' Enrollment Request !';
+        if(@mail($toEmail,$subject,$body,$headers)){
+                $msg = "done";
+        }else{
+                $msg = "error";
+        }
+}
+
+									if($msg == 'done'){
+									echo json_encode('<h5 style="color:green;">Thank you for showing interest with us! Will get back to you shortly.</h5>');
+									 }
+										else if($msg == 'error'){
+									echo json_encode('<h5 style="color:red;">Some problem while sending your queries. Please try again after some time!</h5>');
+									}
+?>
+
---
+++ Enroll_now.php
@@ -0,0 +1,27 @@
+<?php
+$toEmail = "enrollments@itupworks.com";
+$msg = "";
+/* Handling Form Post */
+if($_SERVER['REQUEST_METHOD'] == 'POST' &&  $_POST['name'] !="" &&  $_POST['email'] !="" && $_POST['mobile'] !="" ){
+        $body ="<br/>Name : ". $_POST['name'];
+        $body .="<br/>Email : ". $_POST['email'];
+		$body .="<br/>Mobile : ". $_POST['mobile'];
+		$body .="<br/>Expertise : ". $_POST['course'];
+		$body .="<br/>Educational  : ". $_POST['Educational '];
+		$headers .= "MIME-Version: 1.0\r\n";
+        $headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
+		$subject='Course '.$_POST['course'].' Enrollment Request !';
+		if(@mail($toEmail,$subject,$body,$headers)){
+                $msg = "done";
+        }else{
+                $msg = "error";
+        }
+}
+
+									if($msg == 'done'){
+									echo json_encode('<h6 style="color:green;" >Thank you for showing interest with us! Will get back to you shortly.</h6>');
+									 }
+										else if($msg == 'error'){
+									echo json_encode('<h6 style="color:red;">Some problem while sending your queries. Please try again after some time!</h6>');
+									}
+?>
